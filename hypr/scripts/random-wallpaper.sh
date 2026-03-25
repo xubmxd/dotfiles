f15b5a49065687c879921cd2c1f137368dc10c33
xubmxd <shubhamrajsrivastava2006@gmail.com>
2026-03-25 22:17:13 +0530

    made some changes

--- hypr/scripts/random-wallpaper.sh
+++ hypr/scripts/random-wallpaper.sh
@@ -3,6 +3,9 @@
 # Getting wallpaper
 wall=$(find /home/xubm/Pictures/wallpapers/images/dark -type f -iname "*.jpg" -o -iname "*.jpeg" | shuf -n 1)
 
+# setting wallpaper
+swww img $wall --transition-type fade --transition-step 90 --transition-fps 60
+
 # Copying Selected wallpaper to .cache as current wallpaper
 cp "$wall" ~/.cache/current_wallpaper
 
@@ -10,10 +13,8 @@ cp "$wall" ~/.cache/current_wallpaper
 wal -i $wall -o ~/.local/src/pywalium/generate.sh
 
 # Color for gtk using matugen
-matugen image $wall
+matugen image $wall --source-color-index 0 --type  scheme-vibrant
 
-# setting wallpaper
-swww img $wall --transition-type fade --transition-step 90 --transition-fps 60
 
 # ------Spotify--------
 
