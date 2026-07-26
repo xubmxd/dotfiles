89a6b1b0eb88c6847430f5e8560d42cf8a7240b6
xubmxd <shubhamrajsrivastava2006@gmail.com>
2026-07-26 20:38:25 +0530

    added lsp to neovim

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -24,3 +24,15 @@ transparent_bg()
 vim.api.nvim_create_autocmd("ColorScheme", {
     callback = transparent_bg,
 })
+
+vim.o.updatetime = 300
+
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+    callback = function()
+        vim.diagnostic.open_float(nil, {
+            focusable = false,
+            border = "rounded",
+            source = "always",
+        })
+    end,
+})
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,11 +1,31 @@
 return {
-    "nvim-telescope/telescope.nvim",
-
-    dependencies = {
-        "nvim-lua/plenary.nvim",
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        config = function()
+            require("telescope").setup({})
+        end,
     },
 
-    config = function()
-        require("telescope").setup({})
-    end,
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+        },
+        config = function()
+            local telescope = require("telescope")
+
+            telescope.setup({
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown({}),
+                    },
+                },
+            })
+
+            telescope.load_extension("ui-select")
+        end,
+    },
 }
