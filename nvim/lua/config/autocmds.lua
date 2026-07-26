a371068de2061aae757ab4b1058e7e69c0c48616
xubmxd <shubhamrajsrivastava2006@gmail.com>
2026-07-26 21:24:07 +0530

    added formatters and linters for some programs

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -24,15 +24,37 @@ transparent_bg()
 vim.api.nvim_create_autocmd("ColorScheme", {
     callback = transparent_bg,
 })
-
 vim.o.updatetime = 300
 
-vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+vim.diagnostic.config({
+    virtual_text = true,
+    underline = true,
+    signs = true,
+    update_in_insert = false,
+})
+
+vim.api.nvim_create_autocmd("CursorHold", {
     callback = function()
+        -- Don't show another popup if one is already open
+        for _, winid in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+            local config = vim.api.nvim_win_get_config(winid)
+            if config.relative ~= "" then
+                return
+            end
+        end
+
         vim.diagnostic.open_float(nil, {
             focusable = false,
+            close_events = {
+                "BufLeave",
+                "CursorMoved",
+                "InsertEnter",
+                "FocusLost",
+            },
             border = "rounded",
             source = "always",
+            prefix = "",
+            scope = "cursor",
         })
     end,
 })
