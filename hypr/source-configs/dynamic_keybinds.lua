f9bdfaccb32c98502c6f5c71d88b7bbca916b866
xubmxd <shubhamrajsrivastava2006@gmail.com>
2026-09-06 10:34:55 +0530

    update

--- hypr/source-configs/dynamic_keybinds.lua
+++ hypr/source-configs/dynamic_keybinds.lua
@@ -1,2 +1,3 @@
 local mainMod = "SUPER"
 hl.bind(mainMod .. " + SHIFT + apostrophe", hl.dsp.exec_cmd("quickshell ipc call island toggleWallpaper"))
+hl.bind("ALT+F4", hl.dsp.exec_cmd("quickshell ipc call island togglePower"))
